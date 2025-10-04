f05e5cd4112bc83b59f30f4aa0a2f4f27d74d5e8
ShreyaPL <shreyap.lanjewar@gmail.com>
2025-10-04 13:40:51 -0700

    Final fixes for package build issues

--- my_rb1_description/setup.py
+++ my_rb1_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_rb1_description'
 
@@ -10,14 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='shreya',
+    maintainer='Shreya',
     maintainer_email='shreyap.lanjewar@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    description='RB1 Robot Visualization package',
+    license='MIT',
+    # tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
