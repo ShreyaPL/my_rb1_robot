8300cf3753e124e7ba7b60aeb685014cd4f0d430
ShreyaPL <shreyap.lanjewar@gmail.com>
2025-10-07 00:54:46 -0700

    Add final empty_world.launch.py. Added spawn.launch.py for spawning my_rb1_robot with blue body. No gzebo tags yet.

--- my_rb1_gazebo/setup.py
+++ my_rb1_gazebo/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_rb1_gazebo'
 
@@ -10,14 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='shreya',
     maintainer_email='shreyap.lanjewar@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    description='Package for spawning RB1 in Gazebo',
+    license='MIT',
+    # tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
